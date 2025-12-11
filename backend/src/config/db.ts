820e5cbd4c28601ec49d7623daa54eb14d3ae2ea
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2025-12-11 12:14:24 +0530

    Dockerfile updates

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,28 +1,29 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const { PG_USERNAME, PG_PASSWORD, PG_DATABASE, PG_HOST, PG_PORT } = process.env;
+const DATABASE_URL = process.env.DATABASE_URL;
 
-export const sequelize = new Sequelize(
-  PG_DATABASE as string,
-  PG_USERNAME as string,
-  PG_PASSWORD as string,
-  {
-    host: PG_HOST || "localhost",
-    port: Number(PG_PORT || 5432),
-    dialect: "postgres",
-    logging: false,
-  }
-);
-
-export async function initDb() {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Connected to Postgres");
-  } catch (err) {
-    console.error("❌ DB connection error", err);
-    process.exit(1);
-  }
-}
+export const sequelize = DATABASE_URL
+  ? new Sequelize(DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.PG_DATABASE!,
+      process.env.PG_USERNAME!,
+      process.env.PG_PASSWORD!,
+      {
+        host: process.env.PG_HOST || "localhost",
+        port: Number(process.env.PG_PORT || 5432),
+        dialect: "postgres",
+        logging: false,
+      }
+    );
